5e253e7ac1d5b6ed6458f96b2716f18af4b949d3
pangxiaoqing <xiaoqing246810@163.com>
2018-02-03 15:49:09 +0800

    login

--- script/Controller/login.js
+++ script/Controller/login.js
@@ -1,17 +1,14 @@
 !function(methods){
-    var url_arr = [
-      '../script/Service/AppCommonService.js',
-      '../script/Method/AppCommonMethod.js',
-      '../script/Router/AppCommonRouter.js'
-    ];
 
-    var service = hmd.service;
-    hmd.require(url_arr,function () {
-        /*点击登录事件*/
+        var service = hmd.service;
         var $root = $('#root');
         var $idCard= $("#idCard");
         var $telNum = $("#tel_num");
         var $checkNum = $("#check_num");
+        var $info = $("#info");
+        var $sentCheckBtn = $("#send_check_num");
+
+        $sentCheckBtn.attr("disabled",false);
         /*点击登录事件*/
         function submit() {
             $('#login_submit').click(function(){
@@ -29,96 +26,77 @@
 
 
         /*点击发送验证码事件*/
-        function checkForm() {
-
-        }
-        /*var regIdCard = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
+        var regIdCard = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
         var regTelNum = /^1[1-9]\d{9}$/;
         var obj = {
             regIdCard:regIdCard,
             regTelNum:regTelNum
         }
-
         function idCard() {
-            var id = $("#idCard").val();
+            var id = $idCard.val();
             // console.log(id);
             // console.log(obj.regIdCard.test(id));
             if(id === ""){
-                $("#idCard").focus();
-                $("#info").show().html("请输入您的身份证号码！");
+                // $idCard.focus();
+                $info.show().html("请输入您的身份证号码！");
                 return false;
             }
             if(id && !(obj.regIdCard.test(id))){
-                $("#idCard").focus();
-                $("#info").show().html("请输入正确的身份证号码！");
+                // $idCard.focus();
+                $info.show().html("请输入正确的身份证号码！");
                 return false;
             }
             if(id && obj.regIdCard.test(id)){
-                $("#info").hide();
+                $info.hide();
                 return true;
             }
-            flag = true;
             return true;
         }
         function telNum() {
-            var tel = $("#tel_num").val();
+            var tel = $telNum.val();
             if(tel === ""){
-                $("#tel_num").focus();
-                $("#info").show().html("请输入您手机号！");
+                // $telNum.focus();
+                $info.show().html("请输入您手机号！");
                 return false;
             }
             if(tel && !(obj.regTelNum.test(tel))){
-                $("#tel_num").focus();
-                $("#info").show().html("请输入正确的手机号！");
+                // $telNum.focus();
+                $info.show().html("请输入正确的手机号！");
                 return false;
             }
             if(tel && obj.regTelNum.test(tel)){
-                $("#info").hide();
+                $info.hide();
                 return true;
             }
-            flag2 = true;
             return true;
-        }*/
-        /*判断表单身份证号和手机号正确之后发送验证码可点击，否则不可点击*/
-
-        // console.log(idCard());
-        /*function checkForm(){
-            $("#send_check_num").attr("disabled",true);
-
-            $("#idCard").blur(function () {
-                idCard();
+        }
+        function initInputEvent(){
+            $idCard.on('input propertychange',function(){
+                checkForm();
             });
-            $("#tel_num").blur(function () {
-                telNum();
+            $telNum.on('input propertychange',function(){
+                checkForm();
             });
-            console.log(typeof flag);
-            console.log(flag2);
-            if(flag && flag2){
-                $("#send_check_num").addClass("code_button_cur");
-                $("#send_check_num").attr("disabled",false);
-            }
-            /!*if(idCard()){
-                if(telNum()){
-                    $("#send_check_num").addClass("code_button_cur");
-                    $("#send_check_num").attr("disabled",false);
-                }
-            }*!/
-        }*/
+        }
+        
+        /*$("#idCard").on('change',function(){
+            console.log(12);
+        })*/
 
-        /*function checkForm() {
-            $("#send_check_num").click(function () {
-                $("#send_check_num").addClass("code_button_cur");
-                if(idCard()){
-                    if(telNum()){
-                        $("#idCard").attr("disabled",true);
-                        /!*发送验证码接口*!/
-                        service.checkForm(function (data) {
-                            console.log(data);
-                        })
-                    }
-                }
-            });
-        }*/
+        function checkForm() {
+            console.log(idCard());
+                console.log(telNum());
+            if(idCard() && telNum()){
+                $sentCheckBtn.attr("disabled",false);
+                $sentCheckBtn.addClass("code_button_cur");
+                $sentCheckBtn.click(function(){
+                    $idCard.attr("disabled",true);
+                    $telNum.attr("disabled",true);
+                    alert($telNum.val());
+                })
+            };
+        }
+        
         /*判断在文本框输入时，键盘松开时其他符号输入为空*/
         function keyUpEvent() {
             $idCard.keyup(function () {
@@ -156,9 +134,9 @@
             focusBlur($telNum);
             focusBlur($checkNum);
             keyUpEvent();
-            checkForm();
+            initInputEvent();
+            // checkForm();
             submit();
         }
         init();
-    })
 }(hmd.methods);
