b1fcde7523c5a33161965af40a853e52ff2cb216
pangxiaoqing <xiaoqing246810@163.com>
2018-02-05 17:01:23 +0800

    login.js

--- script/Controller/login.js
+++ script/Controller/login.js
@@ -2,11 +2,13 @@
 
         var service = hmd.service;
         var $root = $('#root');
+        var $contentBox = $("#contentBox");
         var $idCard= $("#idCard");
         var $telNum = $("#tel_num");
         var $checkNum = $("#check_num");
         var $info = $("#info");
         var $sentCheckBtn = $("#send_check_num");
+        var inputReg = $contentBox.find('input[inputReg="reg"]');
 
         $sentCheckBtn.attr("disabled",false);
         /*点击登录事件*/
@@ -25,24 +27,21 @@
         }
 
 
-        /*点击发送验证码事件*/
+        /*---------点击发送验证码事件-----------*/
         var regIdCard = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
         var regTelNum = /^1[1-9]\d{9}$/;
         var obj = {
             regIdCard:regIdCard,
             regTelNum:regTelNum
-        }
+        };
+        /*判断身份证号格式是否正确的idCard方法*/
         function idCard() {
             var id = $idCard.val();
-            // console.log(id);
-            // console.log(obj.regIdCard.test(id));
             if(id === ""){
-                // $idCard.focus();
                 $info.show().html("请输入您的身份证号码！");
                 return false;
             }
             if(id && !(obj.regIdCard.test(id))){
-                // $idCard.focus();
                 $info.show().html("请输入正确的身份证号码！");
                 return false;
             }
@@ -52,6 +51,7 @@
             }
             return true;
         }
+        /*判断手机号格式是否正确的telNum方法*/
         function telNum() {
             var tel = $telNum.val();
             if(tel === ""){
@@ -70,31 +70,36 @@
             }
             return true;
         }
+        /*一个表单实时判断所有表单的值是否为true*/
         function initInputEvent(){
-            $idCard.on('input propertychange',function(){
+            $idCard.keyup(function () {
                 checkForm();
             });
-            $telNum.on('input propertychange',function(){
-                checkForm();
-            });
-        }
-        
-        /*$("#idCard").on('change',function(){
-            console.log(12);
-        })*/
 
+            $telNum.keyup(function () {
+                checkForm();
+            })
+        }
+        /*disabled*/
+        function attrDisabled(id,boolean) {
+            id.attr("disabled",boolean);
+        }
+        /*所有表单的判断*/
         function checkForm() {
             console.log(idCard());
-                console.log(telNum());
+            console.log(telNum());
+
             if(idCard() && telNum()){
-                $sentCheckBtn.attr("disabled",false);
+                attrDisabled($sentCheckBtn,false);
                 $sentCheckBtn.addClass("code_button_cur");
+                attrDisabled($idCard,true);
+                attrDisabled($telNum,true);
                 $sentCheckBtn.click(function(){
-                    $idCard.attr("disabled",true);
-                    $telNum.attr("disabled",true);
+
+
                     alert($telNum.val());
                 })
-            };
+            }
         }
         
         /*判断在文本框输入时，键盘松开时其他符号输入为空*/
@@ -113,30 +118,45 @@
         /*鼠标输入时value消失，鼠标离开时value显示*/
         function focusBlur(id) {
             id.focus(function () {
-                this.value = '';
+                if(this.value === id.attr("value")){
+                    this.value = '';
+                }else {
+                    this.value = this.value;
+                }
             });
             id.blur(function () {
                 if(this.value === ''){
                     this.value = id.attr("value");
+                }else {
+                    this.value = this.value;
                 }
             });
         }
 
-
-
-
+        for(var i=0,len=inputReg.length;i<len;i++){
+            focusBlur($(inputReg[i]));
+        }
 
 
 
         /*初始化*/
         function init(){
-            focusBlur($idCard);
-            focusBlur($telNum);
-            focusBlur($checkNum);
             keyUpEvent();
             initInputEvent();
-            // checkForm();
             submit();
         }
         init();
 }(hmd.methods);
+/*
+for(key in objFocusBlur){
+    objFocusBlur[key].focus(function () {
+        var labelIdCard = $(this).siblings('label');
+        labelIdCard.hide();
+    });
+    objFocusBlur[key].blur(function () {
+        var labelIdCard = $(this).siblings('label');
+        if(this.value === ""){
+            labelIdCard.show();
+        }
+    })
+}*/
